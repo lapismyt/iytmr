428928a95b78230802ff5acf5be93d21da9fcd26
Nikita Ugnich <nikitagavrilin005@gmail.com>
2026-03-22 00:10:13 +0300

    Update handle_inline_query.rs

--- src/bot/handlers/handle_inline_query.rs
+++ src/bot/handlers/handle_inline_query.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use teloxide::{Bot, types::InlineQuery};
 
-use crate::{db::DatabaseHelper, downloader::Downloader};
+use crate::{bot::types::BotWrapped, db::DatabaseHelper, downloader::Downloader};
 
 pub async fn handle_inline_query(
     bot: &BotWrapped,
